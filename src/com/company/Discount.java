7ad2977bf41ed0abb02d502e6f3d3ea1f14ca1a9
sanjanenic <sanjanenicbl@gmail.com>
2022-03-02 13:56:02 +0100

    neues Commit

--- src/com/company/Discount.java
+++ src/com/company/Discount.java
@@ -1,4 +1,32 @@
 package com.company;
 
 public class Discount {
+    public static void main(String[] args) {
+
+        //On the bus you receive a discount if you are a senior citizen, a dog or a student.
+
+        boolean senior = true;
+        boolean dog = true;
+        boolean student= true;
+        boolean ergebnis = senior || dog || student;
+        System.out.println ("Die haben eine Angebote! " + ergebnis);
+
+        //In a bookshop you receive a discount if you buy more than 2 non-fiction books and at least one
+        //fiction book
+
+        boolean NotFictionBooks = true;
+        boolean zahler = true;
+        boolean  FictionBook = true;
+
+        boolean ergebnisBook = (NotFictionBooks && zahler) || (FictionBook && NotFictionBooks);
+        System.out.println (" Du hast eine Angebote für gekaufte Bucherer bekommen! " + ergebnisBook);
+
+        //In a grocery shop you get a discount if you buy one drink, but only if it is not alcoholic.
+
+
+
+
+
+
+    }
 }
