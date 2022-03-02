5578b1b79d725b469c01e5fc76aae8671b514304
sanjanenic <sanjanenicbl@gmail.com>
2022-03-02 13:22:12 +0100

    Erste Commit

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Rectangle {
+    public static void main(String[] args) {
+        int width = 6;
+        int lenght = 4;
+         int ergebnis = (2*width) + (2*lenght) ;
+         System.out.println ("Umfang von diese Rchtechk ist: " + ergebnis);
+
+         int area = width * lenght;
+        System.out.println ("Bereich von diese Rchtechk ist: " + area);
+
+
+    }
 }
