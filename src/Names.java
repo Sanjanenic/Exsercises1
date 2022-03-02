391e14d2990524a67230f4ea9bf95f672275f015
sanjanenic <sanjanenicbl@gmail.com>
2022-03-02 13:09:55 +0100

    Initial commit

---
+++ src/Names.java
@@ -0,0 +1,9 @@
+public class Names {
+    public static void main(String[] args) {
+
+     String name ="Anna" ;
+     String nachname ="Berger";
+ System.out.println ("Diese Person heisst " + name +" "+ nachname + ".");
+
+    }
+}
