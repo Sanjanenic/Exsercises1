c321dff90bb96ed3ca7e8ff115043e6cab698394
sanjanenic <sanjanenicbl@gmail.com>
2022-03-03 09:00:34 +0100

    Final Commit

--- src/Names.java
+++ src/Names.java
@@ -5,5 +5,18 @@ public class Names {
      String nachname ="Berger";
  System.out.println ("Diese Person heisst " + name +" "+ nachname + ".");
 
+ //sad ovo radimo kao metodu
+
+        String firstName ="Ada";
+        String lastName = "Lovelace";
+        System.out.println (firstName + " " + lastName );
+        System.out.println ("method:" + makeFullName(firstName, lastName));
+        System.out.println (makeFullName("Grace", "Hopper"));
+    }
+
+    //metoda ide ispod main metode ne unutra!!!!!
+
+    public static String makeFullName(String firstName, String lastName){
+        return firstName + " " + lastName;
     }
 }
---
+++ src/com/company/Retangle2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Retangle2 {
+
+    public static void main(String[] args) {
+        int width = 6;
+        int lenght = 4;
+        int circumference= 2*(width + lenght);
+        System.out.println ("Umfang von diese Rechteck ist: " + circumference);
+
+    }
+}
