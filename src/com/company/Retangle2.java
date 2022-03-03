33cf6b4f3e17ea98f54ae5d910189d55bb23260d
sanjanenic <sanjanenicbl@gmail.com>
2022-03-03 09:33:15 +0100

    Methode erste  Commit

--- src/com/company/Retangle2.java
+++ src/com/company/Retangle2.java
@@ -3,10 +3,18 @@ package com.company;
 public class Retangle2 {
 
     public static void main(String[] args) {
-        int width = 6;
-        int lenght = 4;
-        int circumference= 2*(width + lenght);
-        System.out.println ("Umfang von diese Rechteck ist: " + circumference);
+        int width= 0;
+        int lenght = 0;
+        //int circumference= 2*(width + lenght);
+        //System.out.println ("Umfang von diese Rechteck ist: " + umfangRecnen(int width, int length));
+        int umfang = umfangRecnen (2, 3);
+         System.out.println (umfangRecnen(2, 3));
 
+    }
+
+    public static int umfangRecnen (int width, int lenght){ //metodhen signature
+         int umfang; //u metodi mram imati ovu novu varijablu, bez nje trebalo bi da moze ali kod mene nije funkcionisalo
+         umfang = 2*(width + lenght);
+        return  umfang;
     }
 }
